8b50dccf5ad649377fb47be515e30db9a5f04c55
Javier Gómez Ochoa <javier.gomez@zealiberia.es>
2026-06-17 16:54:18 +0200

    Fix UIAccessibility

--- Sources/ToastKit/Extensions/UIAccessibiltiy.swift
+++ Sources/ToastKit/Extensions/UIAccessibiltiy.swift
@@ -8,8 +8,8 @@ import Foundation
 import UIKit
 
 extension UIAccessibility {
-    static func setFocusTo(_ object: Any?, inTime: CGFloat = 0.2, notification: UIAccessibility.Notification = .layoutChanged) {
-        if AccessibilityHelper.isVoiceOverRunning {
+    @MainActor static func setFocusTo(_ object: Any?, inTime: CGFloat = 0.2, notification: UIAccessibility.Notification = .layoutChanged) {
+        if UIAccessibility.isVoiceOverRunning {
             DispatchQueue.main.asyncAfter(deadline: .now() + inTime) {
                 UIAccessibility.post(notification: notification, argument: object)
             }
@@ -21,8 +21,8 @@ extension UIAccessibility {
     /// posts an accessibility announcement after a short delay.
     /// This is useful for alerting users with visual impairments to changes in the app's state or other important information.
     /// - Parameter announcement: The message to be spoken by VoiceOver.
-    static func announce(_ announcement: String) {
-        if AccessibilityHelper.isVoiceOverRunning {
+    @MainActor static func announce(_ announcement: String) {
+        if UIAccessibility.isVoiceOverRunning {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 UIAccessibility.post(
                     notification: .announcement,
